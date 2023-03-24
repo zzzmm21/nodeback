ed59b7c326816960ff8237c2d33b05ada1438029
HJ1510 <yhj221004@gmail.com>
2023-03-24 16:47:24 +0900

    feat: faqArticle 수정

--- server/routes/faqBoard.js
+++ server/routes/faqBoard.js
@@ -100,25 +100,30 @@ router.delete('/api/meeting/:no/faqArticle/:id', async (req, res) => {
 });
 
 // 특정 게시글 수정
-router.patch('/api/meeting/:no/faqArticle/:id', async (req, res) => {
-  try {
-    const { title, content, hashtags } = req.body;
-    const articleId = req.params.id;
+router.patch(
+  '/api/meeting/:no/faqArticle/:id',
+  noFileUpload.none(),
+  async (req, res) => {
+    console.log(req.body);
+    try {
+      const { title, content, hashtags } = req.body;
+      const articleId = req.params.id;
 
-    const faqArticle = await FAQArticle.findByIdAndUpdate(
-      articleId,
-      { title, content, hashtags },
-      { new: true }
-    );
+      const faqArticle = await FAQArticle.findByIdAndUpdate(
+        articleId,
+        { title, content, hashtags },
+        { new: true }
+      );
 
-    if (!faqArticle) {
-      return res.status(404).json({ message: 'FAQ article not found' });
+      if (!faqArticle) {
+        return res.status(404).json({ message: 'FAQ article not found' });
+      }
+
+      res.status(200).json({ message: 'FAQ article updated successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-
-    res.status(200).json({ message: 'FAQ article updated successfully' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
   }
-});
+);
 
 module.exports = router;
